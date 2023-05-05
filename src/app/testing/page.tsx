bacd060198ffa8964b7f21ec15c0bfbd6b8765b0
Read Langworthy <rlangworthy@gmail.com>
2023-05-05 16:24:51 -0700

    create testing page

---
+++ src/app/testing/page.tsx
@@ -0,0 +1,17 @@
+
+
+export default function Page() {
+  return(
+    <div>
+      <h1>Testing</h1>
+      <h2>h2</h2>
+      <h3>h3</h3>
+      <h4>h4</h4>
+      <h5>h5</h5>
+      <h6>h6</h6>
+      
+      <iframe src="https://node-backend-u62g.onrender.com/test" width="680" height="480"></iframe>
+      
+    </div>
+  )
+}
